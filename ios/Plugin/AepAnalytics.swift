b30c2a4a1dcb87261e17e561bec1f90281caeb1c
epzee <augusto.mazza@gmail.com>
2021-10-13 18:09:25 -0300

    ios config

--- ios/Plugin/AepAnalytics.swift
+++ ios/Plugin/AepAnalytics.swift
@@ -8,7 +8,7 @@ import AEPSignal
 import AEPServices
 
 @objc public class AepAnalytics: NSObject {
-    @objc public func load() {
+    @objc public func load(appId: String) {
         if (UserDefaults.standard.bool(forKey: "HasLaunchedOnce")) {
             // App already launched
             print("[AEP Plugin] App already launched")
@@ -18,8 +18,6 @@ import AEPServices
             // save flag
             UserDefaults.standard.set(true, forKey: "HasLaunchedOnce")
             UserDefaults.standard.synchronize()
-
-            let appId = "7b7e1e4c7d6a/e34e345e6f5b/launch-f5b515c96664-development"
 
             // register aep
             print("[AEP Plugin] Init Aep with appId \(appId)")
--- ios/Plugin/AepAnalyticsPlugin.swift
+++ ios/Plugin/AepAnalyticsPlugin.swift
@@ -10,6 +10,23 @@ public class AepAnalyticsPlugin: CAPPlugin {
     private let implementation = AepAnalytics()
     
     override public func load() {
-        implementation.load();
+        print("[AEP Plugin] Getting AEPEnabled & AEPAppId keys from plist")
+        
+        let aepEnabled: Bool = Bundle.main.object(forInfoDictionaryKey: "AEPEnabled") as? Bool ?? false
+        
+        if (!aepEnabled) {
+            print("[AEP Plugin] aepEnabled false")
+            return
+        }
+        
+        let appId: String = Bundle.main.object(forInfoDictionaryKey: "AEPAppId") as? String ?? ""
+        
+        if (aepEnabled && appId.isEmpty) {
+            print("[AEP Plugin] aepEnabled but no appId found!")
+        }
+        
+        if (aepEnabled && !appId.isEmpty) {
+            implementation.load(appId: appId);
+        }
     }
 }
